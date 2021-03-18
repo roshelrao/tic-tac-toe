338941be6f317633a80ccc978c20f85ae8a0228f
roshelrao <roshelrao1@gmail.com>
2021-03-18 12:35:07 +0530

    dynamic buttons

--- TicTacToe.py
+++ TicTacToe.py
@@ -2,15 +2,42 @@ from tkinter import *
 
 window = Tk()
 
-button1 =  Button(window,bg="white",width=10)
-button2 =  Button(window,bg="white",width=10)
-button3 =  Button(window,bg="white",width=10)
-button4 =  Button(window,bg="white",width=10)
-button5 =  Button(window,bg="white",width=10)
-button6 =  Button(window,bg="white",width=10)
-button7 =  Button(window,bg="white",width=10)
-button8 =  Button(window,bg="white",width=10)
-button9 =  Button(window,bg="white",width=10)
+def textchange1():
+    button1['text']= 'X'
+
+def textchange2():
+    button2['text']= 'O'
+
+def textchange3():
+    button3['text']= 'O'
+
+def textchange4():
+    button4['text']= 'O'
+
+def textchange5():
+    button5['text']= 'O'
+
+def textchange6():
+    button6['text']= 'O'
+
+def textchange7():
+    button7['text']= 'O'
+
+def textchange8():
+    button8['text']= 'O'
+
+def textchange9():
+    button9['text']= 'O'
+
+button1 =  Button(window,bg="white",width=10,command=textchange1)
+button2 =  Button(window,bg="white",width=10,command=textchange2)
+button3 =  Button(window,bg="white",width=10,command=textchange3)
+button4 =  Button(window,bg="white",width=10,command=textchange4)
+button5 =  Button(window,bg="white",width=10,command=textchange5)
+button6 =  Button(window,bg="white",width=10,command=textchange6)
+button7 =  Button(window,bg="white",width=10,command=textchange7)
+button8 =  Button(window,bg="white",width=10,command=textchange8)
+button9 =  Button(window,bg="white",width=10,command=textchange9)
 
 button1.grid(row=0,column=0, sticky="w", padx=3,pady=2)
 button2.grid(row=0,column=1, sticky="w", padx=3,pady=2)
