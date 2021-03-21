a09f9d44ff9e5476c1c5d1325e57ae416d53d74c
roshelrao <roshelrao1@gmail.com>
2021-03-21 21:51:01 +0530

    added an array

--- TicTacToe.py
+++ TicTacToe.py
@@ -6,27 +6,32 @@ window = Tk()
 clicked=True
 count=0
 
-def textchange1(num):
+board = ["","","","","","","","",""]
+
+def textchange1(btn,index):
     global clicked, count
-    if num['text']=="" and clicked==True:
-        num['text']="X"
+    if btn['text']=="" and clicked==True:
+        btn['text']="X"
+        board[index]="X"
         clicked=False
         count = count+1
 
-    elif num['text']=="" and clicked==False:
-        num['text']="O"
+    elif btn['text']=="" and clicked==False:
+        btn['text']="O"
+        board[index]="O"
         clicked=True
         count=count+1
+    print(board)
     
-button1 =  Button(window,bg="white",width=10,text="",command=lambda:textchange1(button1))
-button2 =  Button(window,bg="white",width=10,text="",command=lambda:textchange1(button2))
-button3 =  Button(window,bg="white",width=10,text="",command=lambda:textchange1(button3))
-button4 =  Button(window,bg="white",width=10,text="",command=lambda:textchange1(button4))
-button5 =  Button(window,bg="white",width=10,text="",command=lambda:textchange1(button5))
-button6 =  Button(window,bg="white",width=10,text="",command=lambda:textchange1(button6))
-button7 =  Button(window,bg="white",width=10,text="",command=lambda:textchange1(button7))
-button8 =  Button(window,bg="white",width=10,text="",command=lambda:textchange1(button8))
-button9 =  Button(window,bg="white",width=10,text="",command=lambda:textchange1(button9))
+button1 =  Button(window,bg="white",width=10,text="",command=lambda:textchange1(button1,0))
+button2 =  Button(window,bg="white",width=10,text="",command=lambda:textchange1(button2,1))
+button3 =  Button(window,bg="white",width=10,text="",command=lambda:textchange1(button3,2))
+button4 =  Button(window,bg="white",width=10,text="",command=lambda:textchange1(button4,3))
+button5 =  Button(window,bg="white",width=10,text="",command=lambda:textchange1(button5,4))
+button6 =  Button(window,bg="white",width=10,text="",command=lambda:textchange1(button6,5))
+button7 =  Button(window,bg="white",width=10,text="",command=lambda:textchange1(button7,6))
+button8 =  Button(window,bg="white",width=10,text="",command=lambda:textchange1(button8,7))
+button9 =  Button(window,bg="white",width=10,text="",command=lambda:textchange1(button9,8))
 
 button1.grid(row=0,column=0, sticky="w", padx=3,pady=2)
 button2.grid(row=0,column=1, sticky="w", padx=3,pady=2)
